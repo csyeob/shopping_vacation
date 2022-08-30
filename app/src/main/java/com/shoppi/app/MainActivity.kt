b4c869f6f515c41e31d6ef094a86a1aa40fe626d
최승엽 <csyeob@gmail.com>
2022-08-30 15:05:27 +0900

    <section4> fragment 실습

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -8,28 +8,7 @@ import androidx.constraintlayout.helper.widget.MotionEffect.TAG
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
-        Log.d(TAG, "OnCreate") // 최초 한번
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "OnRestart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "OnResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "OnPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "OnStop")
+        setContentView(R.layout.activity_main)
     }
 
 }
