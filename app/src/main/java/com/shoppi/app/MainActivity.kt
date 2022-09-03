18296d84ca906d5f624b1d448345ef35ae10d630
최승엽 <csyeob@gmail.com>
2022-09-03 22:06:44 +0900

    <sectio6> Botton Navigation 구성하기

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.constraintlayout.helper.widget.MotionEffect.TAG
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     // section 5 화면 추가 및 앱 호환성 대응 , fragment 추가 및 화면 연결
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
     }
 
 }
